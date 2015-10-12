f3a6108b1d78ace36394fb7a198d7388054cf6b4
Julian Lettner <julian.lettner@gmail.com>
2015-10-12 14:38:02 -0700

    Add stub for HMAC function to make project compile.

---
+++ runtime/objc-hmac.h
@@ -0,0 +1,18 @@
+//
+//  objc-hmac.h
+//  objc
+//
+//  Created by Julian Lettner on 10/7/15.
+//
+//
+
+#ifndef objc_hmac_h
+#define objc_hmac_h
+
+#include <stddef.h>
+#include <stdint.h>
+
+// cls points to struct class_t
+uint32_t hmacByClass(const void* ptr, size_t size, const void* cls);
+
+#endif /* objc_hmac_h */
---
+++ runtime/objc-hmac.mm
@@ -0,0 +1,15 @@
+//
+//  objc-hmac.m
+//  objc
+//
+//  Created by Julian Lettner on 10/7/15.
+//
+//
+
+#include "objc-hmac.h"
+
+// cls points to struct class_t
+uint32_t hmacByClass(const void* ptr, size_t size, const void* cls) {
+    // TODO(yln)
+    return key + 7;
+}
