9d8968c1ffcdf393966a35a2c5591b5dce536bcc
Julian Lettner <julian.lettner@gmail.com>
2015-10-13 15:02:14 -0700

    Stub out HMAC helper methods.

--- runtime/objc-defense.mm
+++ runtime/objc-defense.mm
@@ -9,13 +9,28 @@
 #include "objc-defense.h"
 
 #include <unordered_map>
+#include <iostream>
 
 static std::unordered_map<const void*, uint32_t> hashes;
 
+static void abort(const void* ptr, const void* cls) {
+    std::cerr << "Found corrupted structure " << ptr << " in class " << cls;
+    std::cerr << "\nAborting!\n";
+    abort();
+}
+
+static uint32_t computeHMAC(const void* ptr, size_t size, const void* cls) {
+    return 7;
+}
+
 void updateHMAC(const void* ptr, size_t size, const void* cls) {
-    // TODO(yln)
+    uint32_t hash = computeHMAC(ptr, size, cls);
+    hashes[ptr] = hash;
 }
 
 void verifyHMAC(const void* ptr, size_t size, const void* cls) {
-    // TODO(yln)
+    uint32_t hash = computeHMAC(ptr, size, cls);
+    auto search = hashes.find(ptr);
+    if (search == hashes.end() || search->second != hash)
+        abort(ptr, cls);
 }
