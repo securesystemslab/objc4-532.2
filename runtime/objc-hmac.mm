39d09d7f841dc1d3655ed6c273943b81833b164d
Julian Lettner <julian.lettner@gmail.com>
2015-10-20 11:26:36 -0700

    Implement objc-hmac interface.

--- runtime/objc-hmac.mm
+++ runtime/objc-hmac.mm
@@ -10,14 +10,16 @@
 
 void hmac_init(HMAC_MD5_CTX* ctx) {
     uint64_t secret = 7; // TODO(yln): safely get this
-//    _sasl_hmac_md5_init(ctx, (const unsigned char*)&secret, sizeof(uint64_t));
+    _sasl_hmac_md5_init(ctx, (const uint8_t*)&secret, sizeof(uint64_t));
     // TODO(yln): we could precalculate intermidate state from key for performance.
 }
 
 void hmac_update(HMAC_MD5_CTX* ctx, const void* ptr, size_t size) {
-    
+    _sasl_hmac_md5_update(ctx, (const uint8_t*) ptr, size);
 }
 
 uint64_t hmac_final(HMAC_MD5_CTX* ctx) {
-    return 7;
+    uint64_t digest[2]; // 2 * 8 = 16 = HMAC_MD5_SIZE
+    _sasl_hmac_md5_final((uint8_t*) digest, ctx);
+    return digest[0] ^ digest[1];
 }
