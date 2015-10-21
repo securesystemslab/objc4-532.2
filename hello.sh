aba91de70893eb37ffedd9633bbb24e0addc0ba0
Julian Lettner <julian.lettner@gmail.com>
2015-10-21 11:04:18 -0700

    Add scripts to run test programs.

---
+++ hello.sh
@@ -0,0 +1,10 @@
+export DYLD_INSERT_LIBRARIES=/Users/jlettner/work/objc4-532.2/build/Debug/libobjc.A.dylib
+export OBJC_DISABLE_VTABLES=1
+export OBJC_PRINT_VTABLE_SETUP=1
+export OBJC_PRINT_CLASS_SETUP=1
+export OBJC_PRINT_IVAR_SETUP=1
+export OBJC_DEBUG_NONFRAGILE_IVARS=1
+export OBJC_PRINT_REPLACED_METHODS=1
+export OBJC_PRINT_DEPRECATION_WARNINGS=1
+export OBJC_DEBUG_NIL_SYNC=1
+~/work/HelloWorld/build/Release/HelloWorld
