70b8673fb9abc10e5acc010c247d74dcac3c7461
Julian Lettner <julian.lettner@gmail.com>
2015-11-03 17:30:14 -0800

    Refine initialization of random table (better distribution and slightly faster).

--- runtime/objc-secrets.mm
+++ runtime/objc-secrets.mm
@@ -9,7 +9,7 @@
 #include "objc-secrets.h"
 
 #include "objc-private.h"
-#include <ctime>
+#include <stdlib.h>
 
 #define RANDOM_TABLE_SIZE (6 * 4 + 1048576 * 8) // 1048576 = 2^20
 
@@ -18,11 +18,23 @@ static uint8_t* randomTable;
 //__attribute__((constructor)) static
 void secrets_init() {
     randomTable = (uint8_t*) _malloc_internal(RANDOM_TABLE_SIZE);
-    
-    srand(time(NULL));
-    for (uint64_t i = 0; i < RANDOM_TABLE_SIZE; ++i) {
-        randomTable[i] = (uint8_t)rand();
-    }
+
+//    // unsafe
+//    srand(time(NULL));
+//    for (uint64_t i = 0; i < RANDOM_TABLE_SIZE; ++i) {
+//        randomTable[i] = (uint8_t)rand();
+//    }
+
+//    // much slower
+//    int fd = open("/dev/urandom", O_RDONLY);
+//    assert(fd > 0);
+//    ssize_t size = read(fd, randomTable, RANDOM_TABLE_SIZE);
+//    assert(size == RANDOM_TABLE_SIZE);
+//    close(fd);
+
+    // better (better distribution and slightly faster than rand)
+    // https://stackoverflow.com/questions/2572366/how-to-use-dev-random-or-urandom-in-c
+    arc4random_buf(randomTable, RANDOM_TABLE_SIZE);
 }
 
 extern "C"
@@ -31,5 +43,5 @@ uint8_t* _objc_get_secret_cache_table_ptr() {
 }
 
 uint64_t get_secret_slow_path() {
-    return 17;
+    return ((uint64_t*) randomTable)[7];
 }
